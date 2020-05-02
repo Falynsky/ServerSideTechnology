9a9efd663527c0a798e5ff47762f6bef0b84006d
Falynsky <kamil.falinski@gmail.com>
2020-05-03 01:49:41 +0200

    finalne poprawki

--- src/main/java/com/falynsky/tss4/Tss4Application.java
+++ src/main/java/com/falynsky/tss4/Tss4Application.java
@@ -2,6 +2,9 @@ package com.falynsky.tss4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 public class Tss4Application {
@@ -10,4 +13,15 @@ public class Tss4Application {
         SpringApplication.run(Tss4Application.class, args);
     }
 
+    @Bean
+    public MessageSource messageSource() {
+        final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames("classpath:/tss_pl");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        messageSource.setDefaultEncoding("ISO-8859-2");
+        messageSource.setCacheSeconds(5);
+        return messageSource;
+    }
 }
+
+
